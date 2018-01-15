8c5630340ee9690578b9f53767aa1d2e7105ab71
zyd2001 <zyd200101@outlook.com>
2018-01-16 00:27:54 +0800

    Types and Identifiers

---
+++ examples/_1_1.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+
+public class _1_1
+{
+    public static void main(String[] args)
+    {
+        int integer = 1;
+        char character = 'c';
+        double doubleNumber = 1.1;
+        float floatNumber;
+        boolean bool = false;
+    }
+}
